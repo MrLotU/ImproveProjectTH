589f292cf912011350026e3296273fd2bc8a8ce7
Jari Koopman <j.koopman@jarict.nl>
2018-07-21 10:46:42 +0200

    Add form validation

--- menu/forms.py
+++ menu/forms.py
@@ -1,10 +1,18 @@
 from django import forms
 from django.forms.extras.widgets import SelectDateWidget
+from datetime import datetime
 
 from .models import Menu, Item, Ingredient
 
 class MenuForm(forms.ModelForm):
-
     class Meta:
         model = Menu
         fields = ('season', 'items', 'expiration_date')
+
+    def clean(self):
+        super().clean()
+        expiration_date = self.cleaned_data.get('expiration_date')
+        try:
+            datetime.strptime(expiration_date.__str__(), '%Y-%m-%d')
+        except:
+            raise forms.ValidationError('Please provide a valid date in the format YYYY-MM-DD')
